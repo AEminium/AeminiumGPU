7fc175dd9d5c81f3f301fdee02ac9117527a54dd
Alcides Fonseca <me@alcidesfonseca.com>
2011-07-27 11:37:38 +0100

    Added debug in case of CLException when loading device.

--- src/aeminium/gpu/devices/BestContextDeviceFactory.java
+++ src/aeminium/gpu/devices/BestContextDeviceFactory.java
@@ -1,5 +1,6 @@
 package aeminium.gpu.devices;
 
+import com.nativelibs4java.opencl.CLException;
 import com.nativelibs4java.opencl.JavaCL;
 
 public class BestContextDeviceFactory implements DeviceFactory {
@@ -7,7 +8,11 @@ public class BestContextDeviceFactory implements DeviceFactory {
 	@Override
 	public GPUDevice getDevice() {
 		if (JavaCL.listGPUPoweredPlatforms().length > 0) {
-			return new GPUDevice(JavaCL.createBestContext());
+			try {
+				return new GPUDevice(JavaCL.createBestContext());
+			} catch(CLException e) {
+				return null;
+			}
 		} else {
 			return null;
 		}
