8433dd4717f1379c9981df7eb73d123e61957ea8
Alcides Fonseca <me@alcidesfonseca.com>
2012-04-30 11:53:04 +0100

    Exctracted path building from Templates to utils.

--- src/aeminium/gpu/templates/TemplateWrapper.java
+++ src/aeminium/gpu/templates/TemplateWrapper.java
@@ -1,20 +1,19 @@
 package aeminium.gpu.templates;
 
-import java.io.InputStream;
+import java.io.BufferedReader;
+
+import aeminium.gpu.utils.PathHelper;
 
 public class TemplateWrapper {
-	InputStream is;
 	String fname;
 
 	public TemplateWrapper(String file) {
 		fname = "templates/" + file;
 		
-		// Inside the jar, the path is absolute
-		is = TemplateWrapper.class.getResourceAsStream("/" + fname);
 	}
 
-	public InputStream getInputStream() {
-		return is;
+	public BufferedReader getReader() {
+		return PathHelper.openFile(fname);
 	}
 
 	public String getFileName() {
---
+++ src/aeminium/gpu/utils/PathHelper.java
@@ -0,0 +1,39 @@
+package aeminium.gpu.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class PathHelper {
+
+
+	public static InputStream openFileAsStream(String fname) {
+		// First try inside the Jar
+		InputStream is = PathHelper.class.getResourceAsStream("/" + fname);
+		if (is != null) {
+			return is;
+		}
+
+		// Fallback to local file.
+		File f = new File(fname);
+		if (f.exists()) {
+			try {
+				return new FileInputStream(f);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		System.out.println("Could not load file: " + fname);
+		System.exit(1);
+		return null;
+	}
+
+
+	public static BufferedReader openFile(String fname) {
+		return new BufferedReader(new InputStreamReader(openFileAsStream(fname)));
+	}
+}
