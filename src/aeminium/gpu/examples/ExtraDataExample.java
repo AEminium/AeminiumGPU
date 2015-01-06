caf79c20b40beb54d3d6988be2e16b8c0f330e68
Alcides Fonseca <me@alcidesfonseca.com>
2015-01-06 14:37:44 +0000

    Complemented extra data example.

--- src/aeminium/gpu/examples/ExtraDataExample.java
+++ src/aeminium/gpu/examples/ExtraDataExample.java
@@ -3,6 +3,7 @@ package aeminium.gpu.examples;
 import aeminium.gpu.collections.lists.IntList;
 import aeminium.gpu.collections.lists.PList;
 import aeminium.gpu.operations.functions.LambdaMapper;
+import aeminium.gpu.operations.functions.LambdaReducerWithSeed;
 
 public class ExtraDataExample {
 	public static void main(String[] args) {
@@ -33,6 +34,66 @@ public class ExtraDataExample {
 
 		});
 		System.out.println("The first value is " + input.get(N-1));
+		
+		int sum = input.reduce(new LambdaReducerWithSeed<Integer>() {
 
+			public PList<Integer> m3 = input2;
+			
+			@Override
+			public Integer combine(Integer input, Integer other) {
+				return input + other + m3.get(0);
+			}
+
+			@Override
+			public String getSource() {
+				return "return reduce_input_first + reduce_input_second + m[0];";
+			}
+
+			@Override
+			public Integer getSeed() {
+				return 0;
+			}
+
+		});
+
+		System.out.println("The sum of the first " + N + " numbers is " + sum);
+
+		sum = input.map(new LambdaMapper<Integer, Integer>() {
+
+			public PList<Integer> m1 = input2;
+			
+			@Override
+			public Integer map(Integer input) {
+				return m1.get(input);
+			}
+
+			@Override
+			public String getSource() {
+				return "return m1[input];";
+			}
+
+		}).reduce(new LambdaReducerWithSeed<Integer>() {
+
+			public PList<Integer> m = input2;
+			
+			@Override
+			public Integer combine(Integer input, Integer other) {
+				return input + other + m.get(0);
+			}
+
+			@Override
+			public String getSource() {
+				return "return reduce_input_first + reduce_input_second + m[0];";
+			}
+
+			@Override
+			public Integer getSeed() {
+				return 0;
+			}
+
+		});
+
+		System.out.println("The sum of the first " + N + " numbers is " + sum);
+		
 	}
 }
