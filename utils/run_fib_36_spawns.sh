98f457b649af8dc026654defdd157410a006e6b8
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-04 13:02:13 +0000

    Using 40 in benchmarks

--- utils/run_fib_36_spawns.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export i=36
-
-for w in {2..16}
-do
-  echo "fib $i spawns $w"
-  SPAWNS=$w $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
-done
--- utils/run_fib_36_workers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export i=36
-
-for w in 64 128 256 512 768 1024 2048
-do
-  echo "fib $i workers $w"
-  WORKERS=$w $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
-done
