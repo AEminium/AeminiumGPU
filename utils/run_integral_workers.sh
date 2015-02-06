d7a9f92b2cc57a5070b5de4f6396b4a3e0c5037d
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-06 17:51:18 +0000

    Integral workers

---
+++ utils/run_integral_workers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in 6 7
+do
+
+for w in 1024 2048 4096 8192 16384 32768
+do
+  echo "integral 1 $i gpu workers $i"
+  WORKERS=$w FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral.sh 1 $i | grep -v R
+done
+
+done
