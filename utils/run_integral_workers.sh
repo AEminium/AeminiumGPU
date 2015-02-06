ee9588f734543aadc036e899bbfc549d9954c3cd
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-06 17:58:05 +0000

    Fixed bug in print

--- utils/run_integral_workers.sh
+++ utils/run_integral_workers.sh
@@ -5,7 +5,7 @@ do
 
 for w in 1024 2048 4096 8192 16384 32768
 do
-  echo "integral 1 $i gpu workers $i"
+  echo "integral 1 $i gpu workers $w"
   WORKERS=$w FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral.sh 1 $i | grep -v R
 done
 
