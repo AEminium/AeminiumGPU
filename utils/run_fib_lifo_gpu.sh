df44481ad2985eed62e9e0f74f6f6244481896cc
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-07 12:08:17 +0000

    LIFO

--- utils/run_fib_lifo_gpu.sh
+++ utils/run_fib_lifo_gpu.sh
@@ -1,3 +1,6 @@
-export i=35
-echo "fib $i lifo"
-FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
+#!/bin/bash
+for i in {1..37}
+do
+  echo "fib $i gpu lifo"
+  FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
+done
