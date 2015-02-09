b96bcc72d6cfd422c54e2ad0e546b26bcd00370b
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-09 10:58:57 +0000

    Hybrid updated without fifo

--- utils/run_all_fibs_hybrid.sh
+++ utils/run_all_fibs_hybrid.sh
@@ -2,5 +2,5 @@
 for i in {1..38}
 do
   echo "fib $i hybrid"
-  FIFO=1 $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
+  $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_fib.sh $i | grep -v R
 done
