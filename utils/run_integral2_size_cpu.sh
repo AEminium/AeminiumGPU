580a7799496017b2c695a21cf11e17009c0de092
Alcides Fonseca <me@alcidesfonseca.com>
2015-02-10 13:13:38 +0000

    Updated scripts

--- utils/run_integral2_size_cpu.sh
+++ utils/run_integral2_size_cpu.sh
@@ -2,5 +2,9 @@
 for i in {1..7}
 do
   echo "integral 1 $i cpu"
-  FORCE=CPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral.sh 1 $i | grep -v R
+  FORCE=CPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral2.sh 1 $i | grep -v R
+  sudo /sbin/sysctl -w vm.drop_caches=1 > /dev/null
+  sudo /sbin/sysctl -w vm.drop_caches=2 > /dev/null
+  sudo /sbin/sysctl -w vm.drop_caches=3 > /dev/null
+  sleep 1
 done
--- utils/run_integral2_size_gpu.sh
+++ utils/run_integral2_size_gpu.sh
@@ -2,5 +2,5 @@
 for i in {1..7}
 do
   echo "integral 1 $i gpu"
-  WORKERS=8192 FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral2.sh 1 $i | grep -v R
+  FORCE=GPU $APPLEBIN/bin/_repeat.sh sudo -E bash utils/run_integral2.sh 1 $i | grep -v R
 done
